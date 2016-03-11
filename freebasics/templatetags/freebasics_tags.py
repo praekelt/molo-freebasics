9fc5264394d8523356914da0d92287b531479326
codiebeulaine <codiebeulaine@gmail.com>
2016-03-11 12:53:41 +0200

    changed css to get vars from context

--- freebasics/templatetags/freebasics_tags.py
+++ freebasics/templatetags/freebasics_tags.py
@@ -7,10 +7,12 @@ register = Library()
 @register.template_tag('custom_css.html', takes_context=True)
 def custom_css(context):
     styles = {
-        "body-background-color": settings.CUSTOM_BODY_BACKGROUND_COLOR,
-        "body-font-color": settings.CUSTOM_BODY_FONT_COLOR,
-        "body-font-family": settings.CUSTOM_BODY_FONT,
-        "accent-1": settings.CUSTOM_ACCENT_1,
-        "accent-2": settings.CUSTOM_ACCENT_2
+        "body-background-color": settings.CUSTOM_CSS_BASE_BACKGROUND_COLOR,
+        "body-font-family": settings.CUSTOM_CSS_BODY_FONT_FAMILY,
+        "block-background-color": settings.CUSTOM_CSS_BLOCK_BACKGROUND_COLOR,
+        "block-font-family": settings.CUSTOM_CSS_BLOCK_FONT_FAMILY,
+        "text-transform": settings.CUSTOM_CSS_BLOCK_TEXT_TRANSFORM,
+        "accent-1": settings.CUSTOM_CSS_ACCENT_1,
+        "accent-2": settings.CUSTOM_CSS_ACCENT_2
     }
     return {'styles': styles}
