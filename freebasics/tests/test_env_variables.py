327b8cb161f8ecfff2fe454612e5e14960ff74e0
codiebeulaine <codiebeulaine@gmail.com>
2016-03-11 14:56:06 +0200

    adding tests

---
+++ freebasics/tests/test_env_variables.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from molo.core.tests.base import MoloTestCaseMixin
+from freebasics.views import HomeView
+from freebasics.templatetags import freebasics_tags
+
+
+class EnvTestCase(TestCase, MoloTestCaseMixin):
+
+    def setUp(self):
+        self.mk_main()
+
+    def test_block_ordering(self):
+        with self.settings(BANNER_BLOCK_POSITION=4,
+                           LATEST_BLOCK_POSITION=3,
+                           QUESTIONS_BLOCK_POSITION=2,
+                           SECTIONS_BLOCK_POSITION=1):
+            home = HomeView()
+            context = home.get_context_data()
+            self.assertEquals(context['blocks'][0], (
+                'blocks/sections.html', 1))
+            self.assertEquals(context['blocks'][1], (
+                'blocks/questions.html', 2))
+            self.assertEquals(context['blocks'][2], ('blocks/latest.html', 3))
+            self.assertEquals(context['blocks'][3], ('blocks/banners.html', 4))
+
+    def test_css_vars(self):
+        with self.settings(CUSTOM_CSS_BLOCK_TEXT_TRANSFORM="lowercase",
+                           CUSTOM_CSS_ACCENT_2="red"):
+            styles = freebasics_tags.custom_css(context='')
+            self.assertEquals(styles['styles']['accent-2'], 'red')
+            self.assertEquals(styles['styles']['text-transform'], 'lowercase')
