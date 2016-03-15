144e60b82162962d1e61237db08c75a8d9f97bf6
Milton Madanda <madandat@gmail.com>
2016-03-15 10:09:23 +0200

    use underscores

--- freebasics/templatetags/freebasics_tags.py
+++ freebasics/templatetags/freebasics_tags.py
@@ -7,12 +7,12 @@ register = Library()
 @register.inclusion_tag('custom_css.html', takes_context=True)
 def custom_css(context):
     styles = {
-        "body-background-color": settings.CUSTOM_CSS_BASE_BACKGROUND_COLOR,
-        "body-font-family": settings.CUSTOM_CSS_BODY_FONT_FAMILY,
-        "block-background-color": settings.CUSTOM_CSS_BLOCK_BACKGROUND_COLOR,
-        "block-font-family": settings.CUSTOM_CSS_BLOCK_FONT_FAMILY,
-        "text-transform": settings.CUSTOM_CSS_BLOCK_TEXT_TRANSFORM,
-        "accent-1": settings.CUSTOM_CSS_ACCENT_1,
-        "accent-2": settings.CUSTOM_CSS_ACCENT_2
+        "body_background_color": settings.CUSTOM_CSS_BASE_BACKGROUND_COLOR,
+        "body_font_family": settings.CUSTOM_CSS_BODY_FONT_FAMILY,
+        "block_background_color": settings.CUSTOM_CSS_BLOCK_BACKGROUND_COLOR,
+        "block_font_family": settings.CUSTOM_CSS_BLOCK_FONT_FAMILY,
+        "text_transform": settings.CUSTOM_CSS_BLOCK_TEXT_TRANSFORM,
+        "accent_1": settings.CUSTOM_CSS_ACCENT_1,
+        "accent_2": settings.CUSTOM_CSS_ACCENT_2
     }
     return {'styles': styles}
