e70835e050f6d2b07d846157cc154f34b7cfc33e
codiebeulaine <codiebeulaine@gmail.com>
2016-03-29 12:15:47 +0200

    removing context processor from settings

--- freebasics/settings/base.py
+++ freebasics/settings/base.py
@@ -181,7 +181,6 @@ TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
     'django.core.context_processors.request',
     'molo.core.context_processors.locale',
     'molo.profiles.context_processors.get_profile_data',
-    'freebasics.context_processors.default_forms',
 
 )
 
