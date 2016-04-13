9197637e1c013d1e85217864cd3f559dcd13c603
Saeed Marzban <Saeed.mb8@gmail.com>
2016-04-13 14:35:43 +0200

    added load sections tag

--- freebasics/templatetags/freebasics_tags.py
+++ freebasics/templatetags/freebasics_tags.py
@@ -21,3 +21,11 @@ def custom_css(context):
 @register.simple_tag(takes_context=True)
 def get_site_name(context):
     return settings.SITE_NAME
+
+
+@register.assignment_tag(takes_context=True)
+def load_sections(context):
+    request = context['request']
+    locale_code = context.get('locale_code')
+    qs = request.site.root_page.specific.sections()
+    return [a.get_translation_for(locale_code) or a for a in qs]
