730fbed91a834f255ac5ad3bf9c6ca775086e640
Saeed Marzban <Saeed.mb8@gmail.com>
2016-05-04 10:48:57 +0200

    Added multi-languages support for search

--- freebasics/views.py
+++ freebasics/views.py
@@ -2,11 +2,13 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.conf import settings
+from django.utils.translation import get_language_from_request
 
+from molo.core.utils import get_locale_code
 from molo.core.models import ArticlePage
-from wagtail.wagtailsearch.models import Query
-
 from molo.profiles.views import RegistrationView, ProfilePasswordChangeView
+
+from wagtail.wagtailsearch.models import Query
 
 from forms import FBRegistrationForm, ProfilePasswordChangeForm
 
@@ -14,9 +16,10 @@ from forms import FBRegistrationForm, ProfilePasswordChangeForm
 def search(request, results_per_page=10):
     search_query = request.GET.get('q', None)
     page = request.GET.get('p', 1)
-
+    locale = get_locale_code(get_language_from_request(request))
     if search_query:
-        results = ArticlePage.objects.live().search(search_query)
+        results = ArticlePage.objects.filter(
+            languages__language__locale=locale).live().search(search_query)
         Query.get(search_query).add_hit()
     else:
         results = ArticlePage.objects.none()
