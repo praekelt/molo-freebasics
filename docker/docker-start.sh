d13f7ae771910fb6cec6ed129f4e2a8b22c3ec2b
Milton Madanda <madandat@gmail.com>
2016-05-19 17:11:49 +0200

    remove admin pass for freebasics - we use CAS for auth

--- docker/docker-start.sh
+++ docker/docker-start.sh
@@ -6,11 +6,6 @@ set -e
 ./manage.py collectstatic --noinput
 ./manage.py compress
 
-echo "from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').count():
-    User.objects.create_superuser('admin', 'admin@example.com', 'pass')
-" | ./manage.py shell
-
 echo "=> Starting nginx"
 nginx; service nginx reload
 
