e6dbde84f92f558eeaddc54d8bc87af996cc1567
Codie Roelf <codie@praekelt.com>
2016-05-30 13:55:53 +0200

    pep8

--- freebasics/urls.py
+++ freebasics/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import views as auth_views
 
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
