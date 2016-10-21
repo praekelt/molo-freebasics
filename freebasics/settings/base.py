bfbb67533ccfdcb51b78d8bdcf5559b807f7ac2e
Mitso <mqalaba@gmail.com>
2016-10-21 16:23:29 +0200

    Get django compressor versioning files in dev

--- freebasics/settings/base.py
+++ freebasics/settings/base.py
@@ -382,7 +382,7 @@ LOCALE_PATHS = [
 
 STATIC_ROOT = join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
-
+COMPRESS_OFFLINE = True
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
@@ -391,14 +391,6 @@ STATICFILES_FINDERS = [
 
 MEDIA_ROOT = join(PROJECT_ROOT, 'media')
 MEDIA_URL = '/media/'
-
-
-# Django compressor settings
-# http://django-compressor.readthedocs.org/en/latest/settings/
-
-COMPRESS_PRECOMPILERS = [
-    ('text/x-scss', 'django_libsass.SassCompiler'),
-]
 
 
 # Wagtail settings
